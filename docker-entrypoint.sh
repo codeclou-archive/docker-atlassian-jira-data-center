3c0b4eb44ffe573c672592c5308ed9f07a2de81e
clouless <github@gruenewaldt.net>
2019-08-31 15:03:42 +0200

    remove pre built stuff due to fix JRASERVER-69740

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,26 +8,6 @@ umask u+rxw,g+rwx,o-rwx
 ls -la /
 
 #
-# NODE 1...n
-#
-echo ">> docker-entrypoint: extracting JIRA-HOME"
-rm -rf /jira-home/* | true
-tar xfvj /work-private/jira-home-8.3.0.tar.bz2 -C /jira-home/
-chown -R worker:worker /jira-home
-
-#
-# NODE 1
-#
-if [[ "${NODE_NUMBER}" == "1" ]]
-then
-
-  echo ">> docker-entrypoint: extracting JIRA-SHARED-HOME"
-  rm -rf /jira-shared-home/* | true
-  tar xfvj /work-private/jira-shared-home-8.3.0.tar.bz2 -C /jira-shared-home/
-  chown -R worker:worker /jira-shared-home
-fi
-
-#
 # GENERATE CLUSTER CONF
 #
 env | j2  --format=env /work-private/cluster.properties.jinja2 > /jira-home/cluster.properties
